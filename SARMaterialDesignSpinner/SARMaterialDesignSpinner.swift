837283477589bf28b4bc0dc2a1baceccf9567322
Edward Patel <edward@dyna.mo>
2017-06-22 09:49:13 +0200

    Added swiftlint + swiftlint fixes.

--- SARMaterialDesignSpinner/SARMaterialDesignSpinner.swift
+++ SARMaterialDesignSpinner/SARMaterialDesignSpinner.swift
@@ -21,35 +21,39 @@ class SARMaterialDesignSpinner: UIView {
     var count = 0
     let googleColorsArray = [UIColor.blue, UIColor.red, UIColor.yellow, UIColor.green]
     var timer: Timer!
-    
-    //    MARK: -Initializers
-    override init (frame : CGRect) {
+
+    //    mark: -Initializers
+    override init(frame: CGRect) {
         super.init(frame : frame)
         setup()
     }
-    
-    convenience init () {
+
+    convenience init() {
         self.init(frame:CGRect.zero)
         setup()
     }
-    
+
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)!
         setup()
     }
-    
+
     override func layoutSubviews() {
-        self.progressLayer.frame = CGRect(x: 0, y: 0, width: self.bounds.width, height: self.bounds.height);
+        self.progressLayer.frame = CGRect(x: 0, y: 0, width: self.bounds.width, height: self.bounds.height)
         self.updateProgressLayerPath()
     }
-    
-    //    MARK: -Setup Methods
-    func setup(){
-        timer = Timer.scheduledTimer(timeInterval: kSARRingStrokeAnimationDuration, target: self, selector: #selector(SARMaterialDesignSpinner.handleGoogleMultiColoredTimer), userInfo: nil, repeats: true)
+
+    //    mark: -Setup Methods
+    func setup() {
+        timer = Timer.scheduledTimer(timeInterval: kSARRingStrokeAnimationDuration,
+                                     target: self,
+                                     selector: #selector(SARMaterialDesignSpinner.handleGoogleMultiColoredTimer),
+                                     userInfo: nil,
+                                     repeats: true)
         self.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
         setupProgressLayer()
     }
-    
+
     func setupProgressLayer() {
         progressLayer.strokeColor = UIColor.red.cgColor
         progressLayer.fillColor = nil
@@ -57,31 +61,32 @@ class SARMaterialDesignSpinner: UIView {
         self.layer.addSublayer(progressLayer)
         self.updateProgressLayerPath()
     }
-    
+
     func updateProgressLayerPath() {
         let center = CGPoint(x: self.bounds.midX, y: self.bounds.midY)
         let radius = min(self.bounds.width / 2, self.bounds.height / 2) - self.progressLayer.lineWidth / 2
         let startAngle: CGFloat = 0
-        let endAngle: CGFloat = 2*CGFloat(M_PI)
-        let path = UIBezierPath(arcCenter: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: true)
+        let endAngle: CGFloat = 2*CGFloat(Double.pi)
+        let path = UIBezierPath(arcCenter: center, radius: radius, startAngle: startAngle,
+                                endAngle: endAngle, clockwise: true)
         self.progressLayer.path = path.cgPath
-        
+
         self.progressLayer.strokeStart = 0.0
         self.progressLayer.strokeEnd = 0.0
     }
-    
+
     func handleGoogleMultiColoredTimer() {
-        count += 1;
+        count += 1
         if !enableGoogleMultiColoredSpinner {
             return
         }
-        
+
         let index = count % self.googleColorsArray.count
         let color = self.googleColorsArray[index]
         self.progressLayer.strokeColor = color.cgColor
     }
-    
-    //    MARK: -Accessors
+
+    //    mark: -Accessors
     var lineWidth: CGFloat {
         get {
             return self.progressLayer.lineWidth
@@ -91,7 +96,7 @@ class SARMaterialDesignSpinner: UIView {
             self.updateProgressLayerPath()
         }
     }
-    
+
     var strokeColor: UIColor {
         get {
             return UIColor(cgColor: self.progressLayer.strokeColor!)
@@ -100,69 +105,67 @@ class SARMaterialDesignSpinner: UIView {
             self.progressLayer.strokeColor = newValue.cgColor
         }
     }
-    
-    //    MARK: -Animation Methods
+
+    //    mark: -Animation Methods
     func startAnimating() {
         if self.isAnimating {
             return
         }
-        
+
         let animation = CABasicAnimation(keyPath: "transform.rotation")
         animation.duration = 4
         animation.fromValue = 0
-        animation.toValue = (2 * M_PI)
+        animation.toValue = (2 * Double.pi)
         animation.repeatCount = Float.infinity
         self.progressLayer.add(animation, forKey: kSARRingRotationAnimationKey)
-        
+
         let headAnimation = CABasicAnimation(keyPath: "strokeStart")
         headAnimation.duration = 1
         headAnimation.fromValue = 0
         headAnimation.toValue = 0.25
         headAnimation.timingFunction = self.timingFunction
-        
+
         let tailAnimation = CABasicAnimation(keyPath: "strokeEnd")
         tailAnimation.duration = 1
         tailAnimation.fromValue = 0
         tailAnimation.toValue = 1
-        tailAnimation.timingFunction = self.timingFunction;
-        
+        tailAnimation.timingFunction = self.timingFunction
+
         let endHeadAnimation = CABasicAnimation(keyPath: "strokeStart")
         endHeadAnimation.beginTime = 1
         endHeadAnimation.duration = 0.5
         endHeadAnimation.fromValue = 0.25
         endHeadAnimation.toValue = 1
         endHeadAnimation.timingFunction = self.timingFunction
-        
+
         let endTailAnimation = CABasicAnimation(keyPath: "strokeEnd")
         endTailAnimation.beginTime = 1
         endTailAnimation.duration = 0.5
         endTailAnimation.fromValue = 1
         endTailAnimation.toValue = 1
         endTailAnimation.timingFunction = self.timingFunction
-        
+
         let animations = CAAnimationGroup()
         animations.duration = kSARRingStrokeAnimationDuration
         animations.animations = [headAnimation, tailAnimation, endHeadAnimation, endTailAnimation]
-        animations.repeatCount = Float.infinity;
+        animations.repeatCount = Float.infinity
         self.progressLayer.add(animations, forKey: kSARRingStrokeAnimationKey)
-        
-        
-        self.isAnimating = true;
-        
+
+        self.isAnimating = true
+
     }
-    
+
     func stopAnimating() {
         if !self.isAnimating {
             return
         }
-        
+
         self.progressLayer.removeAnimation(forKey: kSARRingRotationAnimationKey)
         self.progressLayer.removeAnimation(forKey: kSARRingStrokeAnimationKey)
-        self.isAnimating = false;
-        
+        self.isAnimating = false
     }
-    
-    //    MARK: -Memory Methods
+
+    //    mark: -Memory Methods
     deinit {
         //        Release the timer here
         if timer != nil {
--- SARMaterialDesignSpinner/ViewController.swift
+++ SARMaterialDesignSpinner/ViewController.swift
@@ -9,58 +9,56 @@
 import UIKit
 
 public extension Float {
-    public static func random(lower: Float, upper: Float) -> Float {
+    public static func random(_ lower: Float, upper: Float) -> Float {
         let r = Float(arc4random()) / Float(UInt32.max)
         return (r * (upper - lower)) + lower
     }
 }
 
 class ViewController: UIViewController {
-    
+
     @IBOutlet weak var spinner: SARMaterialDesignSpinner!
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.setupSpinner()
-        
+
     }
-    
+
     override func awakeFromNib() {
     }
-    //    MARK: -Setup Methods
+    //    mark: -Setup Methods
     func setupSpinner() {
-        self.spinner.strokeColor = UIColor.blueColor()
+        self.spinner.strokeColor = UIColor.blue
         self.spinner.startAnimating()
     }
-    
-    //    MARK: -Action Methods
-    @IBAction func randomColorButtonTapped(sender: AnyObject) {
+
+    //    mark: -Action Methods
+    @IBAction func randomColorButtonTapped(_ sender: AnyObject) {
         self.spinner.strokeColor = self.randomColor()
     }
-    @IBAction func lineWidthButtonTapped(sender: AnyObject) {
-        self.spinner.lineWidth = CGFloat(Float.random(2, upper: 10));
+
+    @IBAction func lineWidthButtonTapped(_ sender: AnyObject) {
+        self.spinner.lineWidth = CGFloat(Float.random(2, upper: 10))
         //        if self.spinner.lineWidth == 0 { self.spinner.lineWidth = 2 }
     }
-    @IBAction func googleColorButtonTapped(sender: AnyObject) {
+
+    @IBAction func googleColorButtonTapped(_ sender: AnyObject) {
         self.spinner.enableGoogleMultiColoredSpinner = !self.spinner.enableGoogleMultiColoredSpinner
     }
-    
-    
+
     func randomColor() -> UIColor {
         let hue = Float.random(0, upper: 1)
         let saturation = Float.random(0.5, upper: 1)
         let brightness = Float.random(0.5, upper: 1)
         return UIColor(hue: CGFloat(hue), saturation: CGFloat(saturation), brightness: CGFloat(brightness), alpha: 1)
     }
-    
-    //    MARK: -Memory Methods
+
+    //    mark: -Memory Methods
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
-}
 
+}
